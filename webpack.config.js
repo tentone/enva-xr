6f2a2b1241cdc20b82dea7891d9900f6dc7b24d5
tentone <tentone@outlook.com>
2022-03-12 15:21:02 +0000

    Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
+	mode: 'production',
 	entry: './src/main.js',
 	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'src'),
+		filename: 'build.js',
+		path: path.resolve(__dirname, 'build'),
 	},
+	module: {
+		rules: [
+			{
+				test: /\.glsl$/i,
+				use: "raw-loader"
+			}
+		]
+	},
+	optimization: {}
 };
