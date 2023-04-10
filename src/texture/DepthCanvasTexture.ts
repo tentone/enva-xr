a6ab47cd1a529f8ba6acb46ab59bb544abb2b93c
Tentone <tentone@outlook.com>
2023-04-10 10:00:17 +0100

    Depth correct

--- src/texture/DepthCanvasTexture.ts
+++ src/texture/DepthCanvasTexture.ts
@@ -1,13 +1,27 @@
-import {CanvasTexture} from "three";
+import {CanvasTexture, Matrix4, Vector3} from "three";
 
 /**
  * Canvas texture to stored depth data obtained from the WebXR API.
+ * 
+ * The data will be processed using CPU and is expected to be slow.
  */
 export class DepthCanvasTexture extends CanvasTexture
 {
+	/**
+	 * Image data to draw information into the canvas.
+	 */
+	public imageData: ImageData = null;
+
+	/**
+	 * Canvas 2D rendering context.
+	 */
+	public context: CanvasRenderingContext2D = null;
+
 	public constructor(canvas: HTMLCanvasElement | OffscreenCanvas)
 	{
 		super(canvas);
+
+		this.context = this.image.getContext("2d", {willReadFrequently: true});
 	}
 
 	/**
@@ -22,41 +36,53 @@ export class DepthCanvasTexture extends CanvasTexture
 	public updateDepth(depthInfo: XRCPUDepthInformation, near: number, far: number): void
 	{
 		// Adjust size of the canvas to match depth information
-		let canvas = this.image;
-		// 	canvas.width = depthInfo.width;
-		// 	canvas.height = depthInfo.height;
+		const canvas = this.image;
+		if (canvas.width !== depthInfo.width || canvas.height !== depthInfo.height)
+		{
+			canvas.width = depthInfo.width;
+			canvas.height = depthInfo.height;
+		}
 
 		// Get image data
-		let context: CanvasRenderingContext2D = canvas.getContext("2d");
-		let image: ImageData = context.getImageData(0, 0, canvas.width, canvas.height);
+		if (!this.imageData) {
+			this.imageData = this.context.createImageData(canvas.width, canvas.height);
+		}
 		
 		// Raw depth data.
-		// let data = new Int16Array(depthInfo.data);
-		// let matrix = new Matrix().fromArray(normDepthBufferFromNormView.matrix);
+		const normFromView: Matrix4 = new Matrix4().fromArray(depthInfo.normDepthBufferFromNormView.matrix);
+		const viewFromNorm: Matrix4 = normFromView.invert();
+		const inverseDepth = new Vector3(1.0 / depthInfo.width, 1.0 / depthInfo.height, 0.0);
 
-		for (let x = 0; x < canvas.width; x++)
+		for (let x = 0; x < depthInfo.width; x++)
 		{
-			for (let y = 0; y < canvas.height; y++)
-			{
-				// Transform distance into values inside of the [near, far] range.
-				let depth = depthInfo.getDepthInMeters(x / canvas.width, y / canvas.height);
+			for (let y = 0; y < depthInfo.height; y++)
+			{	
+				const coords = new Vector3(x, y, 0)
+				coords.multiply(inverseDepth);
+				coords.applyMatrix4(viewFromNorm);
 
-				// Clamp values
+				if (coords.x < 0.0 || coords.x > 1.0 || coords.y < 0.0 || coords.y > 1.0) {
+					continue;
+				}
+				
+				const depth = depthInfo.getDepthInMeters(coords.x, coords.y);
+
+				// Transform distance into values inside of the [near, far] range.
 				let distance = (depth - near) / (far - near);
 				if (distance > 1.0) {distance = 1.0;}
 				else if (distance < 0.0) {distance = 0.0;}
 
 				// Display depth information as RGB
-				let idx = (x * canvas.width + (canvas.width - y)) * 4;
-				image.data[idx] = Math.ceil(distance * 256);
-				image.data[idx + 1] = Math.ceil(distance * 256);
-				image.data[idx + 2] = Math.ceil(distance * 256);
-				image.data[idx + 3] = 255;
+				const idx = (x * canvas.width + (canvas.width - y)) * 4;
+				this.imageData.data[idx] = Math.ceil(distance * 256);
+				this.imageData.data[idx + 1] = Math.ceil(distance * 256);
+				this.imageData.data[idx + 2] = Math.ceil(distance * 256);
+				this.imageData.data[idx + 3] = 255;
 			}
 		}
 
 		// Update canvas content
-		context.putImageData(image, 0, 0);
+		this.context.putImageData(this.imageData, 0, 0);
 		this.needsUpdate = true;
 	}
 
